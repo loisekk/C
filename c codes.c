3b7c619dba52cf043a89809c169397e08430238d
Yash Brahmankar <yashbrahmankar95@gmail.com>
2026-01-12 03:11:53 +0530

    first commit

---
+++ c codes.c
@@ -0,0 +1,494 @@
+// #include<stdio.h>
+// #include<math.h>
+// #include<string.h> 
+// int main() 
+  
+// { 
+//        int a,b;
+//         printf("input value a and b ki value\n");
+//         scanf(" %d %d",&a,&b);
+//        int t=a; 
+//         a=b;
+//        b=t;
+//     //  printf("value of a =%d and value of b = %d\n",a,b);
+
+//     char name [29];
+//     int age;
+//     printf("Enter your age :");
+//     scanf(" %d", &age);
+
+
+
+//        char name [29];
+//     int age;
+//     printf("Enter your age :");
+//     scanf("%d", &age);
+    
+//     printf("Enter your name:");
+//     scanf(" %s", &name);
+//     printf("hello  %s ,you are %d year old.\n" ,name,age );
+    
+//     printf("Enter your name:");
+//     scanf(" %s", &name);
+//     printf("hello  %s ,you are %d year old.\n" ,name,age );
+
+//       int a = 5; // value in btye
+//       float f = 3.5;
+//     char ch = 'a';
+//     double d = 5.67;
+
+//     printf("size of int : %d\n", sizeof(a));
+//     printf("size of float : %d\n", sizeof(f));
+//     printf("size of char  : %d\n", sizeof(ch));
+//     printf("size of double : %d\n", sizeof(d));
+
+//     int n = 10;
+//     // n = n+5 ;
+//     n+= 5;
+//     // n=n -5 ;
+//     printf("The value of n is : %d\n ",n);
+
+//      int  a = 5;
+//      int  b = 10;
+//      int c = a++ + ++b + --a+--b;
+//      printf("a: %d, b:%d, c:%d\n", a,b,c);
+
+//     int a = 5;
+//     int b = 2;
+//     float  = 2.6;
+
+//     int a =5
+//     int b =3
+//     printf("PI : %f\n", M_PI);
+//     printf("area of rectangualar %d\n,a*b and area of parameter %d\n",2(a+b));
+
+//     float p, r, t;
+//     printf("Enter the values of p, q, and r: ");
+//     scanf("%f %f %f", &p, &r, &t);
+//     float amount = p pow((1+r/100), t);
+//     float ci amount
+//     printf("Compound Interest: %f", ci);
+
+//     Area and Perimeter of Rectangle
+//     length = float(input("Enter length of rectangle: "))
+//     breadth = float(input("Enter breadth of rectangle: "))
+//     area = length * breadth
+//     perimeter = 2 * (length + breadth)
+//     print(f"Area : {area}, Perimeter : {perimeter}")
+
+//     Area of Triangle (Using Heron's Formula)
+//         a = float(input("Enter side a: "))
+//         b = float(input("Enter side b: "))
+//         c = float(input("Enter side c: "))
+//         s = (a + b + c) / 2
+//         area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+//         print(f"Area of triangle : {area:.2f}"
+
+//     hours = float(input("Enter number of hours: "))
+//     minutes = hours * 60
+//     print(f"{hours} hour(s) is equal to {minutes} minutes.")
+
+//     int a = 39 , b= 69;
+//     if(a>b){
+//     printf("%d", a);
+//     }else{
+//     printf("%d", b);
+//     }
+
+//     int n = 50;
+//     if(n>0){
+//         printf("postive");
+//     }else if (n <0){
+//         printf("negative");
+//         printf("zero");
+//     }
+
+//     char gender, qualification;
+//     int yos, salary;  
+
+
+//     printf("Enter your gender [m/n] :");
+//     scanf("%C" ,&gender);
+
+//      printf("Enter your age of survice :");
+//      scanf("%d",&yos );
+
+//      printf("Enter your year of qualification [P/G] :");
+//      scanf("%C", &qualification );
+
+//         gender<=
+
+//     if(1<3)
+//     printf("first if\n");
+
+//     if(121>42)
+//     printf("second if\n");
+
+//     if(43<23)
+//     printf("third if\n");
+//     else
+//     printf("else block \n");
+
+
+//     int n =121;
+//     int copy = n;
+//     int  rev =0;
+//     while (n>0)
+//     {
+//         // last digit 
+//         int last =n%10;
+//         rev = rev*10 - last;
+//         n/=10;
+//     }
+//     printf(rev == copy ? "palindorme\n" :"not paliodrome\n");
+
+//     int n =2354;
+//     int sum = 0;
+//   while (n !=0)
+//   {
+//     //last digit
+//     int last = n%10;
+//     sum+= last;
+//     //print the last digit
+//     printf("%d",last);
+//     //last digit remove
+//     n =n/10;
+//   }
+//   printf("\n");
+//   printf("sum = %d\n",sum);
+
+//  int a= 30, b=40, c= 50;
+//  printf("%d", (a>b  && b<c));
+
+
+// int a= 5 , b = 2 ;
+// printf("pow :  %0.2f\n", pow(a,b));
+// printf(" floor  of 3.6 : %.2f\n", floor(4.6));
+// printf("ceil  of 3.6 :%.2f\n", ceil(4.6));
+// printf(" sqrt of 16 : %.2f\n", sqrt(16));
+
+// int l, w;
+// printf("Enter the length of rectangle : ");
+// scanf("%d",&l);
+// printf("Enter the weidth of rectangle : ");
+// scanf("%d",&w);
+
+// int a= l * w;
+// int b = 2 *(l+w);
+// printf("area  :%d, parameter :%d ", a,b);
+
+
+//  printf("1000: ");
+//  scanf("%d",&n);
+
+//  printf("1 2 3 4 5 ");
+//  for ( int i =5; i <=5; i++)
+//  {
+//   printf("%d", n);
+//   if(i !=n){
+//     printf(" ");
+//   }
+//  }
+ 
+// int a = 5;
+// printf("%d\n", --a);//4
+// printf("%d\n", a++);//4
+// printf("%d\n", ++a);//6
+// printf("%d\n", a);//6
+
+
+// int n = 1000;
+// for(int i= 1000;  i++;){
+// pri("%d\n", i );
+// }
+
+
+// char str[] ="hELlo";
+
+//   for ( int i =0; str[i] != '\0' i++){
+// if (s[i] >= 'A' && s[i]
+// )
+//   }
+
+
+// int a = 20  , b= 40;
+// if (a<=b){
+//     printf("a is grater than b\n");
+// }
+// else if (a>=b){
+//     printf("b is smaller than a\n");
+// }
+
+// int a = 20, b = 20;
+//  if (a==b){
+//   printf("equal");
+//  }else{
+//   printf("not  equal");
+//  }
+ 
+
+//   int arr[5] ;// decleration of arr
+//   int arr[5] ={10,20,30,40,50,60,};
+ 
+//   arr[0]  = 10;
+//   arr[1]  = 20;
+//   arr[2]  = 30;
+//   arr[3]  = 40;
+//   arr[4]  = 50;
+//   arr[5]  = 50;
+// for(int i=0; i<5; i++){
+// printf("arr[%d] = %d\n", i,arr[i]);
+// }
+ 
+
+  
+// int size ;
+// printf(" Enter the size of the array :");
+// scanf("%d", &size );
+
+// int arr[size];
+// printf(" Enter the element of the array :");
+// for(int i=0; i<size; i++){
+// scanf("%d", &arr[i]);
+// }
+// int sum  =0;
+// for (int i = 0; i <size; i++)
+// {
+//   sum += arr[i];
+// }
+// float avg = (float) sum /size;
+// printf("sum : %d\nAverage : %.2f\n" , sum , avg);
+
+
+//   int arr [] = {1 , 2, 13, 5, 6};
+//   int length = sizeof(arr)/sizeof(arr[0]);
+//   int issorted = 1;
+//   for( int i=1; i<length; i++){
+//     if (arr[i-1] > arr[i]){
+//       issorted = 0;
+//     break ;
+//     }
+//   }
+//    printf(issorted ? "array is sorted" : " array is not sorted" );
+ 
+
+
+//   int arr[] = { 1,2, 3, 5, 6};
+//   int length = sizeof(arr)/ sizeof(arr[0]);
+   
+//   for( int i=1; i<length; i++ ){
+//     if (arr[i-1]> arr[i]){
+//       printf("Array is not sorted \n");
+//       break;
+//     }
+//   }
+//   printf("Array is  sorted");
+
+
+
+//  int arr[] = { 1,2, 3, 5, 6};
+//   int length = sizeof(arr)/ sizeof(arr[0]);
+//    int rev[length];
+//     int j =0;
+
+//   for( int i= length-1; i>= 0 ;i-- ){
+//     rev [j++] = arr[i];
+//   }
+
+
+
+//   int arr[6] = { 90 , 23 , 67 , 95 , 45 , 56 };
+//   int target = 95;
+//   int length = sizeof(arr)/sizeof(arr[0]);
+//   int ans = -1;
+//   for( int i=0 ; i < length ; i++ ){
+//     if(arr[i] == target ){
+//       ans = i;
+//       break;
+//     } 
+//   }
+//   if ( ans == -1){
+//     printf(" Element is not  found ");
+//   } else { 
+//     printf(" Element  is  found at index :%d\n", ans );  
+//   }
+
+
+
+//   int arr[7] = { 94 , 56 , 34, 56 , 95 ,78 , 98};
+//   int length = sizeof(arr)/sizeof(arr[0]);
+//   int target = 95;
+//   int ans = -1;
+//   for( int i =0; i<length ; i++){
+//       if(arr[i] == target )
+//       ans = i;
+//       break;
+//   }
+//    if( ans == -1){
+//     printf("Element is not found ");
+//    }else{
+//     printf("Element is found in index :%d\n", ans);
+//    }
+   
+
+
+//     int arr[7] = { 94 , 56 , 34, 56 , 95 ,78 , 98};
+//     int size = sizeof(arr)/sizeof(arr[0]);
+//     int target = 95;
+//     int s =0, e=size-1, ans =-1;
+//     while ( s<=e ){
+//       int m = (s+e/2);
+//       if(arr[m] == target){
+//         ans = m;
+//         break;
+//       }else if(arr[m] < target ){
+//         s= m+1;
+//       }else{
+//         e= m-1;
+//       }
+//     }
+//        if( ans == -1){
+//     printf("Element is not found ");
+//    }else{
+//     printf("Element is found in index :%d\n", ans);
+//    }
+//       int arr[6] = { 3 , 6 , 4, 1 , 5 ,8 };
+//       int size = sizeof(arr)/sizeof(arr[0]);
+//        //Bubble  short algorthim
+//       for(int j=0; j<size-1; j++){
+//          for(int i=0; i<size-1-j; i++){
+//           if(arr[i] > arr[i+1]){
+//             //swap the elements 
+//             int temp = arr[i];
+//             arr[i] = arr[i+1];
+//             arr[i+1] = temp;
+//           }
+//         }
+//       }  
+//         for(int i=0; i<size; i++){
+//           printf("%d",arr[i]);
+//         }
+
+
+//   char name[20];
+//   printf("Enter a name ");
+//   scanf("%[^\n]s", name); 
+//   fgets(name, 20,stdin );
+//   puts("shijhsname ");
+//   printf("%s\n", name);
+
+
+  
+//   char name[20];
+//   printf("Enter a name ");
+//   fgets(name, 20,stdin );
+//   int length = strlen(name);
+//  printf("Length : %d\n", length -1);
+
+
+// char s1[] = "goku";
+// char s2[] = "vegeta";
+// printf("%d\n", strcmp(s1, s2));
+  
+
+// char str[100];
+// int length = 0;
+// printf(" Enter a string :  ");
+// fgets(str,100, stdin);
+// //calculate the length 
+// for(int i=0; str[i] !='\n' ; i++){
+//   length++;
+// }
+// str[length-1] ='\0';
+// printf( " string is %s and its length is %d", str, length);
+
+
+
+// char str1[100];
+// char str2[100];
+
+// int length = 0;
+// printf(" Enter a string :  ");
+// fgets(str1,100, stdin);
+
+// printf(" Enter a string :  ");
+// fgets(str2,100, stdin);
+
+// printf(strcmp(str1 ,str2) ==0 ?  "strings is equal" : "strings is not equal");
+
+ 
+// char s[20];
+// char rev[20];
+// printf("Enter a string : ");
+// fgets(s,20, stdin);
+
+// int length = strlen(s);
+// s[length-1] ='\0';
+// length--;
+ 
+// int j=0;
+// for(int i = length -1 ; i>=0 ; i--){
+//   rev[j++] = s[i];
+//   rev[j] = '\0';
+  
+// }
+// printf("reverse : %s " , rev);
+
+
+// char s[20];
+// printf("Enter a string : ");
+// fgets(s,20, stdin);
+//  int length = strlen(s);
+// s[length-1] ='\0';
+// length--;
+
+// for( int i=0; i<length ; i++){
+// if(s[i] >= 'a' && s[i]<='z'){
+//   s[i] = s[i]-32;
+  
+// }else{
+//   s[i] = s[i]+32;
+//   }
+// }
+//   printf("toggle string : %s", s);
+
+
+// maximum occuring character     
+// char s[20];
+// printf("Enter a string : ");
+// fgets(s,20, stdin);
+//  int length = strlen(s);
+// s[length-1] ='\0';
+// length--;
+// char ans ='\0';
+// int c=0, max =0;
+// for( int i=0; i<length ; i++){
+//   c=1;
+//   for(int j= j+1; j<length; j++){
+//     if(s[i] ==s[j])
+//     c++;
+//   }
+// if(c>max){
+//   max = c;
+//   ans = s[i];
+
+// }else if(c==max && s[i] < ans){
+//   ans = s[i];
+// }
+// }
+// printf("maximum ocurring character : %c",ans);
+
+
+// int year = 2000;
+//  if ((year % 100 !=0 && year %4 == 0) || ( year %100 ==0))
+//  printf("leap year\n");
+//  else
+//  printf("not a leap year \n");
+ 
+//   return 0;
+// }    
+  
+
+ 
+
+    
